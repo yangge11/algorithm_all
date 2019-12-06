72b11283a9e7c16795e3a7447c02b6ce1c42911c
yangge11 <504747754@qq.com>
2019-12-06 17:30:56 +0800

    init

---
+++ sort_demo/__init__.py
@@ -0,0 +1,5 @@
+# @Time    : 19/12/6 16:50
+# @Author  : 504747754@qq.com(ZengYang)
+# @File    : __init__.py.py
+# @Software: PyCharm
+# @ToUse  :
---
+++ sort_demo/sort_demo.py
@@ -0,0 +1,18 @@
+# @Time    : 19/12/6 17:19
+# @Author  : 504747754@qq.com(ZengYang)
+# @File    : sort_demo.py
+# @Software: PyCharm
+# @ToUse  :
+
+def isnert_sort():
+    """
+    插入排序
+    :return:
+    """
+    list1 = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    list2 = []
+    pass
+
+
+if __name__ == '__main__':
+    pass
